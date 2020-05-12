b6ca2ed9fc204723f51bb65f00036012ca9ae13d
juanmafc <juanmafc25@gmail.com>
2020-05-12 07:31:29 -0300

    Added a function to read an user's console input and trigger the getCardChanges function

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { FileUtils } from './FileUtils';
 import { PositionalRectangles,Rectangle } from './PositionalRectangles';
-import { Card } from './LoRSet'
+import { Card as LoRSetCard} from './LoRSet'
+import { Deck, Card } from './Deck'
+import {CardChanges, getCardChanges} from './CardChanges'
 
 
 
@@ -19,18 +21,51 @@ async function getDeckCardsRectangles(): Promise<Rectangle[]> {
 }
 
 
-async function PrintCardsInDeck(): Promise<void> {
-    const cards = FileUtils.readFileToJsonSync<Card[]>(String.raw`C:\Users\juanm\Documents\LoRDeckVersionController\LoRCards.json`);    
 
-    //const codeToNameMap = cards.reduce( (map: any, card:any) => map[card.cardCode] = card.name, {});
-    const codeToNameMap = cards.reduce<Map<string, string>>( (map: Map<string, string>, card:Card) => map.set(card.cardCode, card.name), new Map());
-    //const codeToNameMap = new Map<string, string>( cards.map( (card:Card) => [card.cardCode, card.name] ));
-
-    const cardsRectangles:Rectangle[] = await getDeckCardsRectangles();        
-    cardsRectangles.forEach( (card:Rectangle) => console.log(card.CardCode, codeToNameMap.get(card.CardCode)) );
+function loadCodeToNameCardMap(): Map<string, string>{
+    const setCards = FileUtils.readFileToJsonSync<LoRSetCard[]>(String.raw`C:\Users\juanm\Documents\LoRDeckVersionController\LoRCards.json`);    
+    //const codeToNameMap = setCards.reduce( (map: any, card:any) => map[card.cardCode] = card.name, {});    
+    const codeToNameMap = setCards.reduce<Map<string, string>>(
+        (map: Map<string, string>, card:LoRSetCard) => map.set(card.cardCode, card.name), new Map()
+    );
+    //const codeToNameMap = new Map<string, string>( setCards.map( (card:Card) => [card.cardCode, card.name] ));    
+    return codeToNameMap;
 }
 
-PrintCardsInDeck();
+async function getCurrentDeck(): Promise<Deck> {    
+    const codeToNameMap:Map<string, string> = loadCodeToNameCardMap();
+
+    const cardsRectangles:Rectangle[] = await getDeckCardsRectangles();            
+    
+    let cards: Card[] = cardsRectangles.map( (cardRectangle:Rectangle) => {
+        let cardCode = cardRectangle.CardCode;
+        let cardName = <string> codeToNameMap.get(cardRectangle.CardCode);
+        return new Card(cardCode, cardName);
+    });    
+
+    return new Deck(cards);
+}
+
+
+async function executeUserOption(dataInput: Object) {
+    let option: string = dataInput.toString().trim();    
+    if (option == "1"){        
+        originalDeck = await getCurrentDeck();
+    }
+    else if (option == "2"){        
+        modifiedDeck = await getCurrentDeck();        
+    }
+    else if (option == "3"){
+        console.log( getCardChanges(originalDeck, modifiedDeck) );        
+    }
+}
+
+let stdin = process.openStdin();
+let originalDeck: Deck;
+let modifiedDeck: Deck;
+
+stdin.addListener("data",  executeUserOption );
+
 
 
 //getDeck( deck => console.log(deck.cards) );
