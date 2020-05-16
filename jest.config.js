a605b2ba13de7566fd7a7b71a00c82748ec5dabe
juanmafc <juanmafc25@gmail.com>
2020-05-16 12:49:34 -0300

    Added jest.config.js file, previously ignored by gitignore

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "testEnvironment": "node",
+    "roots": [
+      "./"
+    ],
+    "testMatch": [
+      "**/__tests__/**/*.+(ts|tsx|js)",
+      "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    "transform": {
+      "^.+\\.(ts|tsx)$": "ts-jest"
+    }
+  }
