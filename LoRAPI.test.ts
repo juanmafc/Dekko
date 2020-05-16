a41a23889e0e568381369cf209ec1e328ca5fbcc
juanmafc <juanmafc25@gmail.com>
2020-05-16 14:07:25 -0300

    Corrected the names of the LoRAPI tests

--- LoRAPI.test.ts
+++ LoRAPI.test.ts
@@ -1,26 +1,26 @@
 import { LoRAPI } from './LoRAPI';
 import { ActiveDeckMocks, PositionalRectanglesMocks} from './LoRAPIMocks'
 
-test('Given LoRAPI when getting an empty active deck', async () => {    
+test('Given the LoRAPI, when getting an the active deck outside of a game, an empty deck should be returned', async () => {    
     ActiveDeckMocks.Return(ActiveDeckMocks.emptyActiveDeck);
     const activeDeck = await LoRAPI.getActiveDeck();
     expect(activeDeck).toEqual(ActiveDeckMocks.emptyActiveDeck);
 });
 
 
-test('Given LoRAPI when getting an active deck during a game', async () => {
+test('Given the LoRAPI, when getting the active deck during a game, an active deck with cards should be returned', async () => {
     ActiveDeckMocks.Return(ActiveDeckMocks.activeDeckWithCards);
     const activeDeck = await LoRAPI.getActiveDeck();
     expect(activeDeck).toEqual(ActiveDeckMocks.activeDeckWithCards);
 });
 
-test('Given LoRAPI when getting an postional ectangles in main menu', async () => {
+test('Given the LoRAPI, when getting the postional rectangles in the main menu, empty positional rectangles should be returned', async () => {
     PositionalRectanglesMocks.Return(PositionalRectanglesMocks.emptyPositionalRectangles);
     const positionalRectangles = await LoRAPI.getPositionalRectangles();
     expect(positionalRectangles).toEqual(PositionalRectanglesMocks.emptyPositionalRectangles);
 });
 
-test('Given LoRAPI when getting an postional ectangles in deck menu', async () => {
+test('Given the LoRAPI, when getting the postional rectangles in the deck menu, positional rectangles related to many card should be returned', async () => {
     PositionalRectanglesMocks.Return(PositionalRectanglesMocks.deckMenuPositonalRectanlglesWithCardsInDeck);
     const positionalRectangles = await LoRAPI.getPositionalRectangles();
     expect(positionalRectangles).toEqual(PositionalRectanglesMocks.deckMenuPositonalRectanlglesWithCardsInDeck);
