1937ab0cf0ce93a45d0f08d10e71c6ac3c21c227
juanmafc <juanmafc25@gmail.com>
2020-07-04 15:12:50 -0300

    Added 2 columns to the html to display the removed and added cards

--- renderer.ts
+++ renderer.ts
@@ -1,17 +1,38 @@
 import { ipcRenderer } from 'electron'
 import { Application } from './Application'
+import { CardChanges } from './CardChanges'
+import { Card } from './Deck'
 
 const ipc = ipcRenderer;
 const app = new Application();
 
-document.getElementById('setOriginalDeckButton')!.addEventListener('click', () => {
-    app.setCurrentDeckAsOriginalDeck();
-})
 
-document.getElementById('setModifiedDeckButton')!.addEventListener('click', () => {
-    app.setCurrentDeckAsModifiedDeck();
-})
+initializeDeckActionButtons();
 
-document.getElementById('getCardChangesButton')!.addEventListener('click', () => {
-    console.log(app.getCardChanges());
-})
+function initializeDeckActionButtons() {
+    document.getElementById('setOriginalDeckButton')!.addEventListener('click', () => {
+        app.setCurrentDeckAsOriginalDeck();
+    })
+    
+    document.getElementById('setModifiedDeckButton')!.addEventListener('click', () => {
+        app.setCurrentDeckAsModifiedDeck();
+    })
+    
+    document.getElementById('getCardChangesButton')!.addEventListener('click', () => {
+        renderCardChanges(app.getCardChanges());
+    })
+}
+
+function renderCardChanges(changes: CardChanges) {    
+    renderCardsColumn('RemovedCardsColumn', changes.removedCards);    
+    renderCardsColumn('AddedCardsColumn', changes.addedCards);            
+}
+
+function renderCardsColumn(columnId: string, cards: Card[]) {
+    let column = document.getElementById(columnId);
+    for(let card of cards) {
+        let cardElement = document.createElement("p") as HTMLParagraphElement;
+        cardElement.innerHTML = card.name;
+        column?.appendChild(cardElement);
+    }
+}
